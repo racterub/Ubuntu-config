31527cfa644315050a2725c5893e634bc0912200
racterub <vivi.450@hotmail.com>
2017-10-04 03:12:55 +0000

    fix typo and adding more space for better visibility

--- install.sh
+++ install.sh
@@ -24,8 +24,9 @@ ask DOTFILE "Using Modified Dotfile? [including tons of plugins] (y/n): "
 ask CTF "Deploy CTF environment? (y/n): "
 
 if $UPGRADE; then
-    echo "Updating & Upgrading....."
-    echo ============================
+    echo "+==========================+"
+    echo "|Updating & Upgrading..... |"
+    echo "+==========================+"
     sudo apt-get update
     sudo apt-get -y upgrade
     sudo apt-get -y dist-upgrade
@@ -33,16 +34,21 @@ if $UPGRADE; then
 fi
 
 if $DOTFILE; then
-    echo "Deploying Dotfile & Installing plugins......"
-    echo ===============================================
+    echo "+===========================================+"
+    echo "|Deploying Dotfile & Installing plugins.....|"
+    echo "+===========================================+"
+    
+    #Install essentials
     sudo apt-get install -y build-essential cmake python-dev python-setuptools git bash-completion
     sudo easy_install pip
+
     #Overwrite Dotfile
     cp ./.bashrc ~/
     cp ./.vimrc ~/
     cp ./.tmux.conf ~/
     cp ./.screenrc ~/
     cp ./.gdbinit ~/
+
     #Compile vim from source
     sudo apt-get install -y libncurses5-dev libgnome2-dev libgnomeui-dev \
     libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
@@ -69,13 +75,16 @@ if $DOTFILE; then
     sudo update-alternatives --set editor /usr/bin/vim
     sudo update-alternatives --install /usr/bin/vi vi /usr/bin/vim 1
     sudo update-alternatives --set vi /usr/bin/vim
+    
     #Install POWERLINE
     sudo apt-get install -y powerline
     #Install powerline-status
     sudo pip install powerline-status
+    
     #Install vim plugins
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim/
     vim +PluginInstall +qall
+    
     #Install tmux-memory-status
     git clone https://github.com/racterub/tmux-mem-cpu-load.git ~/.tmux
     cd ~/.tmux/
@@ -91,19 +100,22 @@ if $DOTFILE; then
 fi
 
 if $CTF; then
-    echo "Deploying CTF environment...."
-    echo ================================
+    echo "+==============================+"
+    echo "|Deploying CTF environment.... |"
+    echo "+==============================+"
+    
     #Install multi-arch
     sudo dpkg --add-architecture i386
     sudo apt-get update
     sudo apt-get install -y gcc-multilib
+    
     #Install angr
-    sudo apt-get install python-dev libffi-dev build-essential virtualenvwrapper
+    sudo apt-get -y install python-dev libffi-dev build-essential virtualenvwrapper
     sudo pip install angr --upgrade
-    #Install binwalk
-    sudo apt-get install -y binwalk
-    #Install ltrace,strace,nmap
-    sudo apt-get install -y nmap strace ltrace
+
+    #Install binwalk,ltrace,strace,nmap
+    sudo apt-get install -y binwalk nmap strace ltrace
+    
     #Install z3
     cd ~/
     git clone https://github.com/Z3Prover/z3.git
@@ -112,15 +124,17 @@ if $CTF; then
     cd build
     sudo make
     sudo make install
-    #Install gdb & angelboy's Pwngdb, gdb-peda
+    
+    #Install gdb,  angelboy's Pwngdb & gdb-peda
     sudo apt-get install -y gdb
     cd ~/
     git clone https://github.com/scwuaptx/peda.git ~/.peda/
     git clone https://github.com/scwuaptx/Pwngdb.git ~/.pwngdb/
     echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
     cp ~/.peda/.inputrc ~/
+    
     #Install pwntools
-    sudo apt-get install git python-pip
+    sudo apt-get install -y git python-pip
     cd ~/
     git clone https://github.com/Gallopsled/pwntools.git
     cd pwntools
@@ -130,6 +144,7 @@ if $CTF; then
     cd capstone/
     ./make.sh
     sudo ./make.sh install
+    
     #Install qira
     cd ~/
     wget -qO- https://github.com/BinaryAnalysisPlatform/qira/archive/v1.2.tar.gz | tar zx && mv qira* qira
@@ -140,6 +155,6 @@ if $CTF; then
 fi
 
 echo "===================================="
-echo "        Installation Done           "
+echo "|        Installation Done         |"
 echo "===================================="
 echo "                          - Racterub"
